6fcbec00b5a2d574c014b1462665ee5bd04ed83d
asmfadholi <fadholi.mochammad@moneyforward.co.jp>
2024-12-16 11:28:40 +0900

    fix: open gallery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -327,7 +327,7 @@ export default function Home() {
                 id="fileInput"
                 style={{ display: "none" }}
                 accept="image/*"
-                capture="user"
+                // capture="user"
                 onChange={handleOnChangeFile}
               />
 
